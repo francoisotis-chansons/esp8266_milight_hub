00c24bdffcde97bab9c88dec5ac4009fcf54a7f1
francoisotis-chansons <francois.otis@hotmail.com>
2025-10-20 02:20:23 -0400

    Update PlatformEspressif.h

--- lib/RichHttpServerPatch/PlatformEspressif.h
+++ lib/RichHttpServerPatch/PlatformEspressif.h
@@ -1,1 +1,17 @@
+#pragma once
+#include <FS.h>
+
+#if defined(ARDUINO_ARCH_ESP32) || defined(ESP32)
+  #include <WiFi.h>
+  #include <WebServer.h>
+  #include <SPIFFS.h>
+  using ServerType = WebServer;
+  using Client     = WiFiClient;
+#else
+  #include <ESP8266WiFi.h>
+  #include <ESP8266WebServer.h>
+  #include <LittleFS.h>
+  using ServerType = ESP8266WebServer;
+  using Client     = WiFiClient;
+#endif
 
