e5ef28e3ef5bb91bf92dc0bb58a8574ad7cf5a46
francoisotis-chansons <francois.otis@hotmail.com>
2025-10-20 13:31:25 -0400

    Update RGBConverter.h

--- lib/CompatRGBConverter/RGBConverter.h
+++ lib/CompatRGBConverter/RGBConverter.h
@@ -1,1 +1,9 @@
+#pragma once
+// Shim de compatibilité pour l'ancien header RGBConverter.h
+// Redirige vers la lib maintenue "ColorConverter"
+
+#include <ColorConverter.h>
+
+// Alias de type : tout code qui utilise RGBConverter utilisera ColorConverter
+using RGBConverter = ColorConverter;
 
